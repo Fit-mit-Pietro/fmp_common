1c188fe711fca9f1f9e05183191ebf74f84bab05
paulr <paul.riedlingen@gmail.com>
2021-12-30 12:57:58 +0100

    added ips of aws ec2

--- lib/platform/backend_service/user/user_service_config.dart
+++ lib/platform/backend_service/user/user_service_config.dart
@@ -2,8 +2,9 @@
 class UserServiceConfig{
 
   //final USER_SERVICE_URL = "http://10.0.2.2";
-  final USER_SERVICE_URL = "http://localhost";
+  //final USER_SERVICE_URL = "http://localhost";
   //final USER_SERVICE_URL = "http://3.123.20.40";
+  final SERVICE_URL = "http://3.71.100.125";
   final USER_SERVICE_PORT = "3001";
   final USER_SERVICE_ENDPOINT_USER_REGISTER = "/register";
   final USER_SERVICE_ENDPOINT_USER_LOG_IN = "/login";
