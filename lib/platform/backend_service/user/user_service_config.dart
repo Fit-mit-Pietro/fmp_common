197a847d8bed22dea53e393263fd1b826a70e6c4
paulr <paul.riedlingen@gmail.com>
2021-12-30 16:30:39 +0100

    adapted service ip

--- lib/platform/backend_service/user/user_service_config.dart
+++ lib/platform/backend_service/user/user_service_config.dart
@@ -4,7 +4,7 @@ class UserServiceConfig{
   //final SERVICE_URL = "http://10.0.2.2";
   //final SERVICE_URL = "http://localhost";
   //final SERVICE_URL = "http://3.123.20.40";
-  final SERVICE_URL = "http://3.71.100.125";
+  final SERVICE_URL = "http://18.192.24.164";
   
   final USER_SERVICE_PORT = "3001";
   final USER_SERVICE_ENDPOINT_USER_REGISTER = "/register";
